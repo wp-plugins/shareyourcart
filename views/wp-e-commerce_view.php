1656ffd7fa2d91f87f3619e9f23e5e8a19572cd1
barandisolutions <barandisolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-20 20:32:37 +0000

    v1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/shareyourcart/tags/1.2.1@399385 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/wp-e-commerce_view.php
@@ -0,0 +1,19 @@
+<?php 
+    global $SHAREYOURCART_API; 
+	
+    $plugin_path = get_bloginfo('wpurl') . '/wp-content/plugins/shareyourcart/';
+    
+    $style_file = $plugin_path . 'style.css';
+    $ie_style_file = $plugin_path . 'ie.css';
+    
+    $button_skin = get_option('_shareyourcart_button_skin');
+    $button_position = get_option('_shareyourcart_button_position');
+    $use_iframe = $button_position == 'floating' ? TRUE : FALSE;
+?>
+
+<link rel="stylesheet" href="<?php echo $style_file; ?>" type="text/css"/>
+<!--[if lt IE 9]>
+<link rel="stylesheet" href="<?php echo $ie_style_file ; ?>" type="text/css"/>
+<![endif]-->
+
+<iframe src="<?php echo $SHAREYOURCART_API; ?>/button?client_id=<?php echo urlencode($client_id); ?>&callback_url=<?php echo urlencode(bloginfo('wpurl').'/wp-admin/admin-ajax.php?&action=shareyourcart_wp_e_commerce&'.(isset($product_id) ? 'p='.$product_id : null)) ?>&skin=<?php echo $button_skin;  ?>&orientation=<?php echo $button_position; ?>"<?php if($use_iframe) echo 'class="button_iframe"';else echo 'class="button_iframe-normal"';?>frameBorder="0" scrolling="no"></iframe> 
